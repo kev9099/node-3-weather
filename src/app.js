3ed4f52fc88dc068245eaef6a7e1fe19f6333396
Kevin Lawrence <carkevin1997@gmail.com>
2019-08-15 16:19:38 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,19 +5,20 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+
 const port = process.env.PORT || 3000
 
-// Define paths for Express config
+
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-// Setup handlebars engine and views location
+
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
-// Setup static directory to serve
+
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -100,5 +101,6 @@ app.get('*', (req, res) => {
 
 app.listen(port, () => {
     console.log('Server is up on port' + port)
+
 })
 
